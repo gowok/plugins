3d09427488f628e48ce450fbc417f2e50d3776b0
hadihammurabi <hadihammurabi@gmail.com>
2025-01-04 13:43:58 +0700

    feat(cache): ready to use with memory and redis

---
+++ cache/cache.go
@@ -0,0 +1,73 @@
+package cache
+
+import (
+	"log/slog"
+
+	"github.com/dgraph-io/ristretto"
+	"github.com/eko/gocache/lib/v4/cache"
+	"github.com/eko/gocache/lib/v4/store"
+	store_redis "github.com/eko/gocache/store/redis/v4"
+	store_memory "github.com/eko/gocache/store/ristretto/v4"
+	"github.com/gowok/gowok"
+	"github.com/gowok/gowok/must"
+	"github.com/gowok/gowok/some"
+	"github.com/redis/go-redis/v9"
+)
+
+var caches = map[string]store.StoreInterface{}
+
+func Configure(project *gowok.Project) {
+	configAny, ok := project.ConfigMap["cache"]
+	if !ok {
+		slog.Warn("no configuration", "plugin", "cache")
+		return
+	}
+	configMap, ok := configAny.(map[string]any)
+	if !ok {
+		slog.Warn("no configuration", "plugin", "cache")
+		return
+	}
+	config := ConfigFromMap(configMap)
+
+	for name, dbC := range config {
+		if !dbC.Enabled {
+			continue
+		}
+
+		var client store.StoreInterface
+		if dbC.Driver == "memory" {
+			clientOpt := must.Must(ristretto.NewCache(&ristretto.Config{
+				NumCounters: 1e7,
+				MaxCost:     1 << 30,
+				BufferItems: 64,
+			}))
+			client = store_memory.NewRistretto(clientOpt, store.WithSynchronousSet())
+		} else if dbC.Driver == "redis" {
+			clientOpt := must.Must(redis.ParseURL(dbC.DSN))
+			client = store_redis.NewRedis(redis.NewClient(clientOpt))
+		}
+		caches[name] = client
+	}
+}
+
+func Cache(name ...string) some.Some[*cache.Cache[any]] {
+	n := ""
+	if len(name) > 0 {
+		n = name[0]
+		if db, ok := caches[n]; ok {
+			c := cache.New[any](db)
+			return some.Of(&c)
+		}
+	}
+
+	if n != "" {
+		slog.Info("using default connection", "not_found", n)
+	}
+
+	if db, ok := caches["default"]; ok {
+		c := cache.New[any](db)
+		return some.Of(&c)
+	}
+
+	return some.Empty[*cache.Cache[any]]()
+}
