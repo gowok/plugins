e73316fee9edc6b2b5684c782be94bce8bdeb3f4
hadihammurabi <hadihammurabi@gmail.com>
2025-11-19 17:00:17 +0700

    fix(policy): read gowok config without get

--- policy/config.go
+++ policy/config.go
@@ -4,7 +4,6 @@ import (
 	sqladapter "github.com/Blank-Xu/sql-adapter"
 	redisadapter "github.com/casbin/redis-adapter/v3"
 	"github.com/gowok/gowok"
-	"github.com/gowok/gowok/sql"
 	"github.com/gowok/plugins/cache"
 )
 
@@ -12,14 +11,14 @@ type Option func(*enforcer)
 
 func withAdapter() Option {
 	return func(p *enforcer) {
-		if conf, ok := gowok.Get().Config.SQLs["policy"]; ok {
-			if db, ok := sql.DBNoDefault("policy").Get(); ok {
+		if conf, ok := gowok.Config.SQLs["policy"]; ok {
+			if db, ok := gowok.SQL.GetNoDefault("policy").Get(); ok {
 				p.adapter = gowok.Must(sqladapter.NewAdapter(db, conf.Driver, "casbin_rule"))
 				return
 			}
 		}
 
-		if confs, err := cache.ConfigFromProject(gowok.Get()); err == nil {
+		if confs, err := cache.ConfigFromProject(nil); err == nil {
 			if conf, ok := confs["policy"]; ok {
 				if !conf.Enabled {
 					return
